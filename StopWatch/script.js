e8ada05a377506a68f5098f88a5b3f431e56d753
AnchiTeng <anchi1009@gmail.com>
2022-12-30 14:38:41 +0800

    finished stop watch

--- StopWatch/script.js
+++ StopWatch/script.js
@@ -1,17 +1,20 @@
 const startBtn = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
 const resetBtn= document.getElementById("reset");
-const divTimer = document.getElementById("timer")
+const divTimer = document.getElementById("timer");
+const divTimeStamp = document.getElementById("timestamp");
+//const bg = document.getElementsById("outer_bg");
 
 
 let min = 0;
 let sec = 0;
 let msec = 0;
-
 let timer;
+let timeStamp;
+
 
 let lastStartTime = 0;
-let timeLapsed = 0;
+let timeElapsed = 0;
 
 startBtn.addEventListener("click",start);
 stopBtn.addEventListener("click",stop);
@@ -21,6 +24,8 @@ function start(){
     startBtn.disabled = true;
     stopBtn.disabled = false;
     resetBtn.disabled = false;
+
+    
 
     lastStartTime = Date.now();
     
@@ -34,7 +39,18 @@ function stop(){
     stopBtn.disabled = true;
     resetBtn.disabled = false;
 
-    timeLapsed += Date.now() - lastStartTime;
+    timeElapsed += Date.now() - lastStartTime;
+
+    // Create an "li" node:
+const node = document.createElement("li");
+
+node.textContent = timeStamp;
+
+
+
+    divTimeStamp.appendChild(node);
+
+  
 
     clearInterval(timer);
 
@@ -46,7 +62,12 @@ function reset(){
     resetBtn.disabled = true;
 
     divTimer.textContent = "00:00:000";
-    timeLapsed = 0;
+    timeElapsed = 0;
+    timeStampCount = 0;
+    
+    while (divTimeStamp.lastElementChild) {
+        divTimeStamp.removeChild(divTimeStamp.lastElementChild);
+      }
 
 };
 
@@ -59,6 +80,8 @@ function updateTimer(){
    const secText = formatNum(Math.floor(secElapsed)%60,2);
    const minText = formatNum(Math.floor(minElapsed),2);
 
+   
+timeStamp=`    ${minText} : ${secText} : ${msecText}`;
    divTimer.textContent = `${minText} : ${secText} : ${msecText}`;
 
    
